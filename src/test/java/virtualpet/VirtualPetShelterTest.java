455873f420165427706c505903064bb81f9900ca
David Langford <langfod@gmail.com>
2018-02-02 10:35:34 -0500

    VirtualPetShelter class instantiates

---
+++ src/test/java/virtualpet/VirtualPetShelterTest.java
@@ -0,0 +1,16 @@
+package virtualpet;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VirtualPetShelterTest {
+	
+	VirtualPetShelter  testShelter = new VirtualPetShelter();
+	@Test
+	public void createVirtualPetShelterObject() {
+		
+	}
+
+}
